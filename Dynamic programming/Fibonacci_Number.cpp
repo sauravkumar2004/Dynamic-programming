0d6b1ec9c19face2f520023f22ceba9a11da5299
sauravkumar2004 <121476013+sauravkumar2004@users.noreply.github.com>
2025-02-01 13:28:47 +0530

    Update Fibonacci_Number.cpp

--- Dynamic programming/Fibonacci_Number.cpp
+++ Dynamic programming/Fibonacci_Number.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 vector<int> dp;
 int f(int n){
-    if(n==0 or n==1) return dp[n]=1;
+    if(n==0 or n==1) return dp[n]=n;
     if(dp[n]!=-1) return dp[n];
     return dp[n]= f(n-1)+f(n-2);
 }
